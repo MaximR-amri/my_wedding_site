7289a8d057fac4d35f4c32f6a62848a4b51ab315
Roelens maxim <roelensmaxim@gmail.com>
2022-11-10 19:10:05 +0100

    img fixed

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -6,11 +6,11 @@
     <h3 class="text-center pb-6">Merci om thuis zo warm te laten voelen</h3>
     <div class="text-center">
       <p>Op 10/11/2021</p>
-      <img
+      <!-- <img
         class="rounded-xl shadow-md shadow-slate-600"
         src="src\assets\Wegwerp(36).jpg"
         alt="Nog even wachten, hier een poep"
-      />
+      /> -->
     </div>
   </div>
 </template>
